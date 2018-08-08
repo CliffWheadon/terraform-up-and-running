9f76e27274efd0c6351527700b47d8a1dd588d08
CliffWheadon <cliffwheadon@hotmail.com>
2018-08-08 08:34:38 -0300

    Adding in infrastructure from Chapter 3

---
+++ ch3/stage/data-stores/mysql/main.tf
@@ -0,0 +1,22 @@
+#Using a proper backend instead of TerraGrunt as noted in chapter 3
+terraform {
+  backend "s3" {
+    bucket = "cliffwheadon-terraform-up-and-running-state"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+  engine            = "mysql"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database"
+  username          = "admin"
+  password          = "${var.db_password}"
+  skip_final_snapshot = true
+}
