1ae94c4024f1ff43d12919ccf360a9ba317ff90f
CliffWheadon <cliffwheadon@hotmail.com>
2018-08-11 16:36:10 -0300

    Finishing chapter 5

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -14,7 +14,7 @@ module "webserver_cluster" {
   source = "../../../../modules/services/webserver-cluster"
 
   ami         = "ami-40d28157"
-  server_text = "New server text"
+  server_text = "Hello World!"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "cliffwheadon-terraform-up-and-running-state"
