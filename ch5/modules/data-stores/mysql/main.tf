94284fe983224f7a5b943a46337bac4245d88b3f
CliffWheadon <cliff.wheadon@gmail.com>
2018-08-11 14:36:16 -0300

    ditching per chapter folder structure ... will branch per chapter

--- ch5/modules/data-stores/mysql/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_db_instance" "example" {
-  engine            = "mysql"
-  allocated_storage = 10
-  instance_class    = "db.t2.micro"
-  name              = "${var.db_name}"
-  username          = "admin"
-  password          = "${var.db_password}"
-  skip_final_snapshot = true
-}
--- ch5/modules/data-stores/mysql/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "db_name" {
-  description = "The name of the database"
-}
-
-variable "db_password" {
-  description = "The password for the database"
-}
